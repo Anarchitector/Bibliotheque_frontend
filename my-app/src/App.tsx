66a0993976c28aea5ce36ddba7ecae8bc6acbc24
Andrii Gudz <andrey.gudz@gmail.com>
2024-07-26 15:58:08 +0200

    initial commit

---
+++ my-app/src/App.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+const App = () => {
+  return (
+    <BrowserRouter>
+     <p>New project</p>
+    </BrowserRouter>
+  )
+}
+
+export default App
