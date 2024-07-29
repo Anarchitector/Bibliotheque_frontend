0126bad689c3ed7a93b879993af0234586569ed3
EmptyChair <romaswebex01@gmail.com>
2024-07-29 10:57:46 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+const App = () => {
+  return (
+    <BrowserRouter>
+     <p>New project</p>
+     <p>Home page</p>
+    </BrowserRouter>
+  )
+}
+
+export default App
