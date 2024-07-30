ad61b404f14ae151275337972c1d1a8df7fbbb8b
EmptyChair <romaswebex01@gmail.com>
2024-07-30 14:46:14 +0200

    new version

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { AuthorizationForm } from "pages/UserRegistration/styles"
-import UserLogin from "pages/UserLogin/UserLogin"
+import UserLogin from "components/UserLogin/UserLogin"
 
 const App = () => {
   return ( 
---
+++ src/components/UserLogin/styles.ts
@@ -0,0 +1,39 @@
+import { NavLink } from 'react-router-dom';
+import styled from "@emotion/styled"
+
+
+
+export const FormUserLoginContainer = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items: center;
+  @media (max-width: 600px) {
+    gap:10px;   
+  }
+`
+
+export const FormTitle = styled.h2``
+
+export const InputForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 400px;
+  @media (max-width: 600px) {
+    width: 260px;   
+  }
+`
+
+export const LinkComponent = styled.a`
+  font-size: 16px;
+  text-decoration: none;
+  color: inherit; /* Использует текущий цвет текста */
+
+  &:hover {
+    text-decoration: underline; /* Добавляет подчеркивание при наведении */
+    color: #4a90e2; /* Замените на нужный цвет */
+  }
+  @media (max-width: 600px) {
+    font-size: 14px;
+  }
+`
