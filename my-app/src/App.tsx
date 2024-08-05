a3271581cd7c3f6da2a2acc841867bc54fcf0c3a
EmptyChair <romaswebex01@gmail.com>
2024-08-05 14:18:13 +0200

    addded front profile page components

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,6 +2,7 @@ import Layout from "components/Layout/Layout"
 import Home from "pages/Home/Home"
 import LibraryRegistr from "pages/LibraryRegistr/LibraryRegistr"
 import Login from "pages/Login/Login"
+import PagePersonalCabinet from "pages/PagePersonalCabinet/PagePersonalCabinet"
 import UserRegistr from "pages/UserRegistr/UserRegistr"
 import { BrowserRouter, Routes, Route, } from "react-router-dom"
 
@@ -14,6 +15,7 @@ const App = () => {
       <Route path="/api/auth/register" element={<UserRegistr />} />
       <Route path="/api/auth/login" element={<Login />} />
       <Route path="/api/bibliotek/register" element={<LibraryRegistr />} />
+      <Route path="/api/users/id" element={<PagePersonalCabinet />} />
       <Route path='*' element='Page not found' />
       </Routes>
      </Layout>
